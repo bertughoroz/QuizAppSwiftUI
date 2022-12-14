e0cc4105d23e52288d2dfb6ad6b39f31aec5cfb1
Bertug <ber2horoz@gmail.com>
2022-12-14 20:25:19 +0300

    Initial Commit

---
+++ QuizAppSwiftUI/QuizAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizAppSwiftUIApp.swift
+//  QuizAppSwiftUI
+//
+//  Created by Bertuğ Horoz on 14.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct QuizAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
